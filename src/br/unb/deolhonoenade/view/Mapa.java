f932f3045385a8d3589b48e086ccbb50dff717a8
Omar Júnior <omarroinuj@gmail.com>
2015-03-30 17:48:44 -0300

    Applying identation on Mapa view

--- src/br/unb/deolhonoenade/view/Mapa.java
+++ src/br/unb/deolhonoenade/view/Mapa.java
@@ -17,10 +17,11 @@ import android.view.ViewGroup;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 
-public class Mapa extends Activity {
-
+public class Mapa extends Activity
+{
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_mapa);
 
@@ -35,24 +36,29 @@ public class Mapa extends Activity {
         List<String> medias = new ArrayList<String>();
         
         
-        for(int i = 0; i<todasUfs.size(); i++){
-        	
+        for(int i = 0; i<todasUfs.size(); i++)
+        {	
         	String mediasE = new String();
-        	try {
-				mediasE = String.valueOf( controller.mediaEstado(todasUfs.get(i), codCurso));
-        		
-			} catch (Exception e) {
-				Log.e(this.getClass().toString(), todasUfs.get(i)+"Estado nao existe");
+        	try
+        	{
+				mediasE = String.valueOf( controller.mediaEstado(todasUfs.get(i),
+						codCurso));
+			}
+        	
+        	catch (Exception e)
+        	{
+				Log.e(this.getClass().toString(), todasUfs.get(i)+
+						"Estado nao existe");
 				mediasE="0.0";
 			}
         	
         	medias.add(mediasE);
-        	
         }
         
         String content = "<html>"
                 + "<head>"
-                + "<script type='text/javascript' src='https://www.google.com/jsapi'></script>"
+                + "<script type='text/javascript'"
+                + "src='https://www.google.com/jsapi'></script>"
                 + "<script type='text/javascript'>"
                 + "google.load('visualization', '1', {'packages': ['geochart']});"
                 + "google.setOnLoadCallback(drawRegionsMap);"
@@ -87,8 +93,10 @@ public class Mapa extends Activity {
                 + "['Sergipe', " + medias.get(25) + "],"
                 + "['Tocantins', " + medias.get(26) + "],"            
                 + "]);"
-                + "var chart = new google.visualization.GeoChart(document.getElementById('chart_div'));"
-                + "chart.draw(data, {width: 1280, height: 720, region: 'BR', resolution: 'provinces'});"
+                + "var chart = new google.visualization.GeoChart("
+                + "document.getElementById('chart_div'));"
+                + "chart.draw(data, {width: 1280, height: 720, region: 'BR',"
+                + "resolution: 'provinces'});"
                 + "};"
                 + "</script>"
                 + "</head>"
@@ -97,54 +105,67 @@ public class Mapa extends Activity {
                 + "</body>" 
                 + "</html>";;
         
-        
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webview.requestFocusFromTouch();
         
-        //Usando o método abaixo (loadDataWithBaseURL) não é necessário o arquivo .html da pasta assets.
-        //Sendo assim é executado um "html" em tempo real usando a string 'content' acima.
+        //Usando o método abaixo (loadDataWithBaseURL) não é necessário o arquivo
+        //.html da pasta assets.
+        //Sendo assim é executado um "html" em tempo real usando a string 'content'
+        //acima.
         //Só que não consegui fazer este funcionar.
-        webview.loadDataWithBaseURL( "file:///android_asset/", content, "text/html", "utf-8", null );
+        webview.loadDataWithBaseURL( "file:///android_asset/", content, "text/html",
+        		"utf-8", null );
         
-        //webview.loadUrl("file:///android_asset/Mapa.html"); // Can be used in this way too.
+        //webview.loadUrl("file:///android_asset/Mapa.html"); // Can be used in this
+        //way too.
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.mapa, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_settings)
+		{
 			return true;
 		}
+		
+		else
+		{
+			// Nothing to do
+		}
+		
 		return super.onOptionsItemSelected(item);
 	}
 
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
+	public static class PlaceholderFragment extends Fragment
+	{
+		public PlaceholderFragment()
+		{
+			// Nothing to do
 		}
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
+				Bundle savedInstanceState)
+		{
 			View rootView = inflater.inflate(R.layout.fragment_mapa, container,
 					false);
 			return rootView;
 		}
 	}
-
 }
