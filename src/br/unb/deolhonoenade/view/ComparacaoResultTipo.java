961f33092dae11e730e9c7f881d4bbb8da524abf
Omar Júnior <omarroinuj@gmail.com>
2015-03-30 01:14:35 -0300

    Applying identation on ComparacaoResultTipo view

--- src/br/unb/deolhonoenade/view/ComparacaoResultTipo.java
+++ src/br/unb/deolhonoenade/view/ComparacaoResultTipo.java
@@ -19,15 +19,16 @@ import br.unb.br.deolhonoenade.graphs.holographlibrary.BarGraph;
 import br.unb.deolhonoenade.R;
 import br.unb.deolhonoenade.controller.ControllerCurso;
 
-public class ComparacaoResultTipo extends Activity {
-
+public class ComparacaoResultTipo extends Activity
+{
 	private ControllerCurso controller;
 	private List<String> result = new ArrayList<String>();
 	private float resultado1, resultado2;
 	private String estado1, estado2, tipo1, tipo2;
 	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_comparacao_result_tipo);
 		
@@ -43,71 +44,80 @@ public class ComparacaoResultTipo extends Activity {
 		String eT1 = (estado1 + " " + tipo1);
 		String eT2 = (estado2 + " " + tipo2);
 		
-			TextView estadoTipo1 = (TextView) findViewById(R.id.estadoTipo1);
-			estadoTipo1.setText(eT1);
-			
-			TextView estadoTipo2 = (TextView) findViewById(R.id.estadoTipo2);
-			estadoTipo2.setText(eT2);
-			
-			String resultado1String = String.valueOf(resultado1);
-			String resultado2String = String.valueOf(resultado2);
-			
-			ArrayList<Bar> points = new ArrayList<Bar>();
-			Bar d = new Bar();
-			d.setColor(Color.parseColor("#99CC00"));
-			d.setName("Estado/Tipo 1:");
-			d.setValue(Float.parseFloat(resultado1String.substring(0, 5)));
-			Bar d2 = new Bar();
-			d2.setColor(Color.parseColor("#FFBB33"));
-			d2.setName("Estado/Tipo 2:");
-			d2.setValue(Float.parseFloat(resultado2String.substring(0, 5)));
-			points.add(d);
-			points.add(d2);
-			
-			BarGraph g = (BarGraph)findViewById(R.id.graph3);
-			g.setBars(points);
-			g.setUnit(" ");
+		TextView estadoTipo1 = (TextView) findViewById(R.id.estadoTipo1);
+		estadoTipo1.setText(eT1);
+		
+		TextView estadoTipo2 = (TextView) findViewById(R.id.estadoTipo2);
+		estadoTipo2.setText(eT2);
+		
+		String resultado1String = String.valueOf(resultado1);
+		String resultado2String = String.valueOf(resultado2);
+		
+		ArrayList<Bar> points = new ArrayList<Bar>();
+		Bar d = new Bar();
+		d.setColor(Color.parseColor("#99CC00"));
+		d.setName("Estado/Tipo 1:");
+		d.setValue(Float.parseFloat(resultado1String.substring(0, 5)));
+		Bar d2 = new Bar();
+		d2.setColor(Color.parseColor("#FFBB33"));
+		d2.setName("Estado/Tipo 2:");
+		d2.setValue(Float.parseFloat(resultado2String.substring(0, 5)));
+		points.add(d);
+		points.add(d2);
+		
+		BarGraph g = (BarGraph)findViewById(R.id.graph3);
+		g.setBars(points);
+		g.setUnit(" ");
 
-			g.setContentDescription("Tipo de Instituição " + tipo1 + " no estado " + estado1 + ".   nota: " + String.format("%.3f", resultado1)
-					+ ". E tipo de instituição " + tipo2 + " no estado " + estado2 + ".   nota: " + String.format("%.3f", resultado2));
-
+		g.setContentDescription("Tipo de Instituição " + tipo1 + " no estado " + estado1 + ".   nota: " + String.format("%.3f", resultado1)
+				+ ". E tipo de instituição " + tipo2 + " no estado " + estado2 + ".   nota: " + String.format("%.3f", resultado2));
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.comparacao_result_tipo, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
 		// Handle action bar item clicks here. The action bar will
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_settings)
+		{
 			return true;
 		}
+		
+		else
+		{
+			// Nothing to do
+		}
+		
 		return super.onOptionsItemSelected(item);
 	}
 
 	/**
 	 * A placeholder fragment containing a simple view.
 	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
+	public static class PlaceholderFragment extends Fragment
+	{
+		public PlaceholderFragment()
+		{
+			// Nothing to do
 		}
 
 		@Override
 		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
+				Bundle savedInstanceState)
+		{
 			View rootView = inflater.inflate(
 					R.layout.fragment_comparacao_result_tipo, container, false);
 			return rootView;
 		}
 	}
-
 }
