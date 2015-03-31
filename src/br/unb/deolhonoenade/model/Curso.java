7ca9ada343632a84d61d097a66bd86f356f60095
Matheus S. Pereira <matheussilva.fga@gmail.com>
2015-03-30 21:55:47 -0300

    Writing comments on Curso model

--- src/br/unb/deolhonoenade/model/Curso.java
+++ src/br/unb/deolhonoenade/model/Curso.java
@@ -15,6 +15,8 @@ public class Curso
 	private float conceitoEnade;
 	private String uf;
 
+	/* Constructor to receive all the attributes as parameters and initialize the
+	 * object Curso*/
 	public Curso(int id,
 				 int id_ies,
 				 String nome,
@@ -35,66 +37,79 @@ public class Curso
 		this.IES = IES;
 	}
 
+	// Method to return the content of attribute uf
 	public String getUf()
 	{
 		return uf;
 	}
 
+	// Method to set a content on variable uf
 	public void setUf(String uf)
 	{
 		this.uf = uf;
 	}
 
+	// Method to return the content of attribute id_ies
 	public int getId_ies()
 	{
 		return id_ies;
 	}
 
+	// Method to return the content of attribute id
 	public int getId()
 	{
 		return id;
 	}
 
+	// Method to set a content on variable id
 	public void setId(int id)
 	{
 		this.id = id;
 	}
 
+	// Method to return the content of attribute IES
 	public Instituicao getIES()
 	{
 		return IES;
 	}
 
+	// Method to set a content on variable IES
 	public void setIES(Instituicao iES)
 	{
 		IES = iES;
 	}
 
+	// Method to return the content of attribute nome
 	public String getNome()
 	{
 		return nome;
 	}
 
+	// Method to set a content on variable nome
 	public void setNome(String nome)
 	{
 		this.nome = nome;
 	}
 
+	// Method to return the content of attribute numEstudantes
 	public int getNumEstudantes()
 	{
 		return numEstudantes;
 	}
 
+	// Method to set a content on variable numEstudantes
 	public void setNumEstudantes(int numEstudantes)
 	{
 		this.numEstudantes = numEstudantes;
 	}
 
+	// Method to return the content of attribute numEstudantesInscritos
 	public int getNumEstudantesInscritos()
 	{
 		return numEstudantesInscritos;
 	}
 
+	// Method to set a content on variable numEstudantesInscritos
 	public void setNumEstudantesInscritos(int numEstudantesInscritos)
 	{
 
@@ -109,21 +124,25 @@ public class Curso
 
 	}
 
+	// Method to return the content of attribute municipio
 	public String getMunicipio()
 	{
 		return municipio;
 	}
 
+	// Method to set a content on variable municipio
 	public void setMunicipio(String municipio)
 	{
 		this.municipio = municipio;
 	}
 
+	// Method to return the content of attribute conceitoEnade
 	public float getConceitoEnade()
 	{
 		return conceitoEnade;
 	}
 
+	// Method to set a content on variable conceitoEnade
 	public void setConceitoEnade(float conceitoEnade)
 	{
 		this.conceitoEnade = conceitoEnade;
