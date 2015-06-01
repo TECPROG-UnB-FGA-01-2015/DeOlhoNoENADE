85fe46000c474dc0f9b4aa40fd9eace94f357314
Omar Junior <omarroinuj@gmail.com>
2015-06-01 17:41:04 -0300

    Standardizing comments on model Institution

--- src/model/Institution.java
+++ src/model/Institution.java
@@ -10,15 +10,14 @@ import java.util.ArrayList;
 
 public class Institution
 {
-	private ArrayList<Course> Courses = new ArrayList<Course>();
+	private ArrayList<Course> Courses = new ArrayList<Course>(); // Describes a list of courses of the Institution
 
-	private String name; // Holds the info of the institution's name
-	private String academicOrganization; // Holds the info of the institution's academic organization
-	private String type; // Holds the info of the institution's type
-	private int institutionCode; // Holds the info of the institution's code
+	private String name; // Describes the name of the Institution
+	private String academicOrganization; // Describes the academic organization of the Institution
+	private String type; // Describes the type of the Institution
+	private int institutionCode; // Describes the code of the Institution
 
-	/* Constructor to receive all the attributes as parameters and initialize the
-	 * object Institution*/
+	// Constructor to receive all the attributes as parameters and initialize the object Institution
 	public Institution(String name,
 					   String academicOrganization,
 					   String type,
@@ -31,67 +30,67 @@ public class Institution
 		this.institutionCode = institutionCode;
 	}
 
-	// Method to insert a class to attribute Courses
+	// Insert a class to attribute Courses
 	public void addCourse(Course course)
 	{
 		this.Courses.add(course);
 	}
 
-	// Method to return the content of attribute Courses
+    // Access and returns the property courses
 	public ArrayList<Course> getCourses()
 	{
 		return Courses;
 	}
 
-	// Method to set a content on variable Courses
+    // Sets a new value for the courses property
 	public void setCourses(ArrayList<Course> cursos)
 	{
 		Courses = cursos;
 	}
 
-	// Method to return the content of attribute name
+    // Access and returns the property name
 	public String getName()
 	{
 		return name;
 	}
 
-	// Method to set a content on variable name
+    // Sets a new value for the name property
 	public void setName(String name)
 	{
 		this.name = name;
 	}
 
-	// Method to return the content of attribute academicOrganization
+    // Access and returns the property academicOrganization
 	public String getAcademicOrganization()
 	{
 		return academicOrganization;
 	}
 
-	// Method to set a content on variable academicOrganization
+    // Sets a new value for the academicOrganization property
 	public void setAcademicOrganization(String academicOrganization)
 	{
 		this.academicOrganization = academicOrganization;
 	}
 
-	// Method to return the content of attribute type
+    // Access and returns the property type
 	public String getType()
 	{
 		return type;
 	}
 
-	// Method to set a content on variable type
+    // Sets a new value for the type property
 	public void setType(String type)
 	{
 		this.type = type;
 	}
 
-	// Method to return the content of attribute institutionCode
+    // Access and returns the property institutionCode
 	public int getInstitutionCode()
 	{
 		return institutionCode;
 	}
 
-	// Method to set a content on variable institutionCode
+    // Sets a new value for the institutionCode property
 	public void setInstitutionCode(int institutionCode)
 	{
 		this.institutionCode = institutionCode;
